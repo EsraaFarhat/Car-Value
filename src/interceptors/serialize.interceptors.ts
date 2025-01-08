5c44ed0249479a26ee04b368edda6bc064f69276
EsraaFarhat <esraafarhat97@gmail.com>
2025-01-08 18:07:26 +0200

    Refactor - Exclude Password Field From Response with Custom Interceptor

---
+++ src/interceptors/serialize.interceptors.ts
@@ -0,0 +1,19 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { map, Observable } from 'rxjs';
+import { UserDto } from 'src/users/dtos/user.dto';
+
+export class SerializeInterceptors implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((data: any) => {
+        return plainToInstance(UserDto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,5 @@
 import {
   Body,
-  ClassSerializerInterceptor,
   Controller,
   Delete,
   Get,
@@ -11,6 +10,7 @@ import {
   Query,
   UseInterceptors,
 } from '@nestjs/common';
+import { SerializeInterceptors } from 'src/interceptors/serialize.interceptors';
 import { createUserDto } from './dtos/create-user.dto';
 import { updateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -24,7 +24,7 @@ export class UsersController {
     return this.usersService.create(body.email, body.password);
   }
 
-  @UseInterceptors(ClassSerializerInterceptor)
+  @UseInterceptors(SerializeInterceptors)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
