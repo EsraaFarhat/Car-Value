50bedf44d4821d772432c260db822745dd99686c
EsraaFarhat <esraafarhat97@gmail.com>
2025-06-23 22:19:29 +0300

    Implement getEstimate Request

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,9 +1,11 @@
 import {
   Body,
   Controller,
+  Get,
   Param,
   Patch,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { AdminGuard } from 'src/guards/admin.guard';
@@ -11,14 +13,20 @@ import { AuthGuard } from 'src/guards/auth.guard';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
-import { CreateReportDto } from './create-report-dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
+import { CreateReportDto } from './dtos/create-report-dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { ReportDto } from './dtos/report.dto';
 import { ReportsService } from './reports.service';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private reportService: ReportsService) {}
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    return this.reportService.createEstimate(query);
+  }
 
   @Post()
   @UseGuards(AuthGuard)
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -2,7 +2,8 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/user.entity';
 import { Repository } from 'typeorm';
-import { CreateReportDto } from './create-report-dto';
+import { CreateReportDto } from './dtos/create-report-dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { Report } from './report.entity';
 
 @Injectable()
@@ -10,6 +11,22 @@ export class ReportsService {
   constructor(
     @InjectRepository(Report) private repository: Repository<Report>,
   ) {}
+
+  createEstimate({ make, model, year, lng, lat, mileage }: GetEstimateDto) {
+    return this.repository
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where(' make = :make', { make })
+      .andWhere(' model = :model', { model })
+      .andWhere(' lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere(' lat - :lat BETWEEN -5 AND 5', { lat })
+      .andWhere(' year - :year BETWEEN -3 AND 3', { year })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawOne();
+  }
 
   create(reportDto: CreateReportDto, user: User) {
     const report: Report = this.repository.create(reportDto);
