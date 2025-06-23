bf44969457ee0a49f7b40751be841efab389fb42
EsraaFarhat <esraafarhat97@gmail.com>
2025-06-23 18:37:30 +0300

    Fix Type Definition Error

--- src/middlewares/current-user.middleware.ts
+++ src/middlewares/current-user.middleware.ts
@@ -1,15 +1,22 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
+import { User } from 'src/users/user.entity';
 import { UsersService } from 'src/users/users.service';
+
+declare global {
+  interface ExpressRequest extends Request {
+    currentUser?: User;
+  }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
   constructor(private userService: UsersService) {}
-  async use(req: Request, res: Response, next: NextFunction) {
+  async use(req: ExpressRequest, res: Response, next: NextFunction) {
     const { userId } = req.session || {};
     if (userId) {
       const user = await this.userService.findOne(userId);
-      // @ts-expect-error: currentUser is not defined on Request type but is added dynamically for authentication
+
       req.currentUser = user;
     }
     next();
